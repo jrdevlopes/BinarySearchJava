88a4024b3dcd9511fdf0aa3e8a8ee2a40fd120b7
jrdevlopes <jrdevlopes@gmail.com>
2022-07-07 14:47:36 -0300

    Init

---
+++ src/br/com/joao/search/Main.java
@@ -0,0 +1,25 @@
+package br.com.joao.search;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    // Binary Search
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random gerador = new Random();
+
+        try{
+            int num = gerador.nextInt(500);
+
+
+
+
+
+
+        }finally{
+            sc.close();
+        }
+    }
+}
